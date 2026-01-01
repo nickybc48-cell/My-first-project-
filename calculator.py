735f8bd62150f3c00a41e65e8c507e5393e8c6e4
nickybc48-cell <nickybc48@gmail.com>
2026-01-01 11:03:57 +0200

    Add simple calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# Simple calculator
+num1 = 10
+num2 = 5
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
